8aa9c32df513ab0249016c8c0ae1c643972d6483
ACking-you <2394626220@qq.com>
2022-11-27 21:54:23 +0800

    add:添加jsoncpp测试

--- main.cpp
+++ main.cpp
@@ -1,26 +1,32 @@
 #include "time.hpp"
 #include <iostream>
+#include <filesystem>
 #include <fstream>
 using namespace std;
 #include "nlohmann/json.hpp"
 using json_nl = nlohmann::json;
 
+#include"json/json.h"
+using Value = Json::Value;
+using Reader = Json::Reader;
+
 #include "Json.h"
-using namespace yazi::json;
 
 #include "my-json/Parser.h"
 using my_parser = json::Parser;
 
+//用于调整测试和验证文件的路径
+#define BASE_DIR  "../../"
 
 //输出到valid.json文件方便查看验证是否解析正确
 void outPutValidJson(std::string const& src){
-  auto ofs = ofstream("../valid.json");
+  auto ofs = ofstream(BASE_DIR"valid.json");
   ofs<<src;
 }
 
 //获取用于测试的json数据
 std::string getSourceString(){
-  auto ifs = ifstream("../test.json");
+  auto ifs = ifstream(BASE_DIR"test.json");
   if(ifs){
     return string{istreambuf_iterator<char>(ifs),istreambuf_iterator<char>()};
   }
@@ -32,7 +38,7 @@ void testYaziJson()
 {
   //get src string
   auto data = getSourceString();
-  Json json;
+  yazi::json::Json json;
   //start bench
   {
     Timer t;
@@ -57,6 +63,24 @@ void testNlohmannJson()
   outPutValidJson(j.dump());
 }
 
+//测试jsoncpp的表现
+void testJsonCpp(){
+  //get src string
+  auto data = getSourceString();
+  Value j;
+  Reader r;
+  //start bench
+  {
+    Timer t;
+    if(!r.parse(data,j)){
+      std::cerr<<"jsoncpp 出错";
+      return;
+    }
+  }
+  //valid string
+  outPutValidJson(j.toStyledString());
+}
+
 //测试我的json解析库的表现
 void testMyJson(){
     //get src string
@@ -73,9 +97,13 @@ void testMyJson(){
 
 int main()
 {
+
   for(int i=0;i<10;i++){
     testYaziJson();
     testMyJson();
+    testJsonCpp();
     testNlohmannJson();
+    std::cout<<"-----------------------"<<std::endl;
   }
+
 }
